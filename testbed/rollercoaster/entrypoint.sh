a46809363be112fdd977d119c5e07741ed46d330
Jakob Schlyter <jakob@kirei.se>
2023-04-13 13:08:58 +0200

    init

---
+++ testbed/rollercoaster/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ! -f /config/nsd_control.key ]; then
+	nsd-control-setup
+fi
+
+echo "Starting NSD..."
+nsd $NSD_OPTIONS
+
+echo "Starting webserver..."
+lighttpd -f /etc/lighttpd/lighttpd.conf
+
+echo "Running DNS Rollercoaster..."
+rollercoaster-signer --config-file=/config/rollercoaster.toml --loop
